b11e32b17b5f33628c43e32553a38d01c05b2f79
jyoh5 <ojy1018@gmail.com>
2023-07-19 18:51:37 +0900

    nested routes

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,51 +1,201 @@
-import { useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Route, Switch, useLocation, useParams } from "react-router-dom";
 import { styled } from "styled-components";
+import Price from "./Price";
+import Chart from "./Chart";
+
+
+// components START =======================================
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 const Container = styled.div`
-    padding: 0px 20px;
-    max-width: 480px;
-    margin: 0 auto;
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
-    height: 10vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 20px;
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Title = styled.h1`
-    font-size: 48px;
-    color: ${props => props.theme.accentColor};
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
 `;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-
-const Loader = styled.span`
-    text-align: center;
-    display: block;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
 `;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+// components END =========================================
 
+
+// interface START ========================================
 interface RouteParams {
     coinId: string;
 }
 interface RouteState {
     name: string;
 }
-
+interface ITag{
+    coin_counter: number;
+    ico_counter:  number;
+    id: string;
+    name: string;
+}
+interface ITeam{
+    id: string;
+    name:  string;
+    position: string;
+}
+interface ILinksExtended{
+    type: string;
+    url:  string;
+}
+interface InfoData {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+    ogo: string,
+    tags: ITag[],
+    team: ITeam[],
+    description: string,
+    message: string,
+    open_source: boolean,
+    started_at: string,
+    development_status: string,
+    hardware_wallet: boolean,
+    proof_type: string,
+    org_structure: string,
+    hash_algorithm: string,
+    links_extended: ILinksExtended[],
+    first_data_at: string,
+    last_data_at: string
+}
+interface PriceData {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    circulating_supply: number,
+    total_supply: number,
+    max_supply: number,
+    beta_value: number,
+    first_data_at: string,
+    last_updated: string,
+    quotes: {
+        USD: {
+            price: number,
+            volume_24h: number,
+            volume_24h_change_24h: number,
+            market_cap: number,
+            market_cap_change_24h: number,
+            percent_change_15m: number,
+            percent_change_30m: number,
+            percent_change_1h: number,
+            percent_change_6h: number,
+            percent_change_12h: number,
+            percent_change_24h: number,
+            percent_change_7d: number,
+            percent_change_30d: number,
+            percent_change_1y: number,
+            ath_price: number,
+            ath_date: string,
+            percent_from_price_ath: number,
+        }
+    }
+}
+// interface END ==========================================
 
 function Coin() {
     const [loading, setLoading] = useState(true);
     const {coinId} = useParams<RouteParams>();
     const {state} = useLocation<RouteState>();
-    console.log(state);
+    const [info, setInfo] = useState<InfoData>();
+    const [priceInfo, setPrice] = useState<PriceData>();
+    useEffect(() => {
+        (async() => {
+            const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+            const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+            setInfo(infoData);
+            setPrice(priceData);
+            setLoading(false);
+        })();
+    }, [coinId]);
     return (
         <Container>
             <Header>
-                <Title>Detail: {state?.name || "Loading"}</Title>
+                <Title>
+                    {state?.name ? state.name : loading ? "Loading..." : info?.name}
+                </Title>
             </Header>
-            {loading ? <Loader>Loading...</Loader> : null}
+            {loading ? (
+        <Loader>Loading...</Loader>
+        ) : (
+            <>
+                <Overview>
+                    <OverviewItem>
+                    <span>Rank:</span>
+                    <span>{info?.rank}</span>
+                    </OverviewItem>
+                    <OverviewItem>
+                    <span>Symbol:</span>
+                    <span>${info?.symbol}</span>
+                    </OverviewItem>
+                    <OverviewItem>
+                    <span>Open Source:</span>
+                    <span>{info?.open_source ? "Yes" : "No"}</span>
+                    </OverviewItem>
+                </Overview>
+                <Description>{info?.description}</Description>
+                <Overview>
+                    <OverviewItem>
+                    <span>Total Suply:</span>
+                    <span>{priceInfo?.total_supply}</span>
+                    </OverviewItem>
+                    <OverviewItem>
+                    <span>Max Supply:</span>
+                    <span>{priceInfo?.max_supply}</span>
+                    </OverviewItem>
+                </Overview>
+                <Switch>
+                    <Route path={`/${coinId}/price`}>
+                        <Price></Price>
+                    </Route>
+                    <Route path={`/${coinId}/chart`}>
+                        <Chart></Chart>
+                    </Route>
+                </Switch>
+            </>
+        )}
         </Container>
     );
 }
