eb19dfa77c7298c2195a75ed7803daccb1fed7c3
jyoh5 <ojy1018@gmail.com>
2023-07-19 14:59:16 +0900

    setup for react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import Circle from "./Circle";
+import { createGlobalStyle } from "styled-components";
+import Router from "./Router";
 
+// 전체에(global) 스타일 적용
+// head에 주입함
+const GlobalStyle = createGlobalStyle`
+    body {
+        color:goldenrod;
+    }
+`;
 
+// <></> => fragment
 function App() {
-  return (
-    <div>
-      <Circle bgColor="teal" borderColor="black"></Circle>
-      <Circle bgColor="tomato" text="second"></Circle>
-    </div>
-  );
+    return (
+        <>
+            <GlobalStyle></GlobalStyle>
+            <Router></Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/routes/Coins.tsx
@@ -0,0 +1,5 @@
+function Coins() {
+    return <h1>Main</h1>
+}
+
+export default Coins;
