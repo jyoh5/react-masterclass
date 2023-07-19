631b52d82336116486510426c6418140beea46c0
jyoh5 <ojy1018@gmail.com>
2023-07-19 17:57:14 +0900

    move to detail page

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,53 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
+`;
+
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+`;
+
+const Title = styled.h1`
+    font-size: 48px;
+    color: ${props => props.theme.accentColor};
+`;
+
+
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
 
 interface RouteParams {
     coinId: string;
 }
+interface RouteState {
+    name: string;
+}
+
 
 function Coin() {
+    const [loading, setLoading] = useState(true);
     const {coinId} = useParams<RouteParams>();
-    return <h1>Detail: {coinId}</h1>
+    const {state} = useLocation<RouteState>();
+    console.log(state);
+    return (
+        <Container>
+            <Header>
+                <Title>Detail: {state?.name || "Loading"}</Title>
+            </Header>
+            {loading ? <Loader>Loading...</Loader> : null}
+        </Container>
+    );
 }
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -25,7 +25,8 @@ const Coin = styled.li`
     a {
         padding: 20px;
         transition: color 0.2s ease-in;
-        display: block;
+        display: flex;
+        align-items: center;
     }
     &:hover {
         a {
@@ -44,6 +45,12 @@ const Title = styled.h1`
 const Loader = styled.span`
     text-align: center;
     display: block;
+`;
+
+const Img = styled.img`
+    width: 25px;
+    height: 25px;
+    margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -78,7 +85,13 @@ function Coins() {
                 <CoinsList>
                     {coins.map(coin => (
                         <Coin key={coin.id}>
-                            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                            <Link to={{
+                                pathname: `/${coin.id}`,
+                                state: {name: coin.name},
+                            }}>
+                                <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
+                                {coin.name} &rarr;
+                            </Link>
                         </Coin>
                     ))}
                 </CoinsList>
