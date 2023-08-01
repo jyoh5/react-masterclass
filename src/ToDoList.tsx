4cd5e79a0116bb2b33465a1113c11635a27a391c
jyoh5 <ojy1018@gmail.com>
2023-08-01 14:20:23 +0900

    reac-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,20 +1,70 @@
-import React, { useState } from "react";
+import React from "react";
+import {useForm} from "react-hook-form";
+
+
+interface iForm {
+    toDo: string;
+    toDoSecond: string;
+    email: string;
+    extraError?: string;
+}
 
 function ToDoList() {
-    const [toDo, setToDo] = useState("");
-    const onChange = (e:React.FormEvent<HTMLInputElement>) => {
-        const {currentTarget: {value}} = e;
-        setToDo(value);
+    const { register, handleSubmit, formState:{errors}, setError } = useForm<iForm>({
+        defaultValues: {
+            email: "@naver.com",
+        }
+    });
+    const onValid = (data:iForm) => {
+        if(data.toDo !== data.toDoSecond){
+            setError("toDoSecond", {message: "toDoSecond are not the same."}, {shouldFocus: true});
+        }
+        // setError("extraError", {message: "Server offline."});
     };
-    const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log(toDo);
-    };
+    console.log(errors);
+    // const [toDo, setToDo] = useState("");
+    // const [toDoError, setToDoError] = useState("");
+    // const onChange = (e:React.FormEvent<HTMLInputElement>) => {
+    //     const {currentTarget: {value}} = e;
+    //     setToDoError("");
+    //     setToDo(value);
+    // };
+    // const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault();
+    //     if (toDo.length < 10){
+    //         return setToDoError("To do should be longer");
+    //     }
+    //     console.log(toDo);
+    // };
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input placeholder="Write a to do" value={toDo} onChange={onChange} />
+            <form onSubmit={handleSubmit(onValid)} style={{display:"flex", flexDirection: "column" }}>
+                <input {...register("toDo", {
+                        required: true, 
+                        minLength: 10, 
+                        validate: {
+                            noNico: (value) => value.includes("nico") ? "No nico" : true,
+                            noNaco: (value) => value.includes("naco") ? "No naco" : true,
+                        }})
+                    } 
+                    placeholder="Write a to do" />
+                <span>{errors?.toDo?.message}</span>
+                <input {...register("toDoSecond", {
+                        required: "toDoSecond is required.", 
+                        minLength: { value: 10, message: "Your toDoSecond is too short."}})
+                    } 
+                    placeholder="Write a to do" />
+                <span>{errors?.toDoSecond?.message}</span>
+                <input {...register("email", {
+                        required: "Email is required.", 
+                        pattern: { 
+                            value: /^[A-Za-z0-9._%+-]+@naver.com$/, 
+                            message: "Only naver.com emails allowed." }})
+                    } 
+                    placeholder="Write a Email" />
+                <span>{errors?.email?.message}</span>
                 <button>Add</button>
+                <span>{errors?.extraError?.message}</span>
             </form>
         </div>
     );
