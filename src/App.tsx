78fd4d758bd2632c19e4c779f8222fe039379647
jyoh5 <ojy1018@gmail.com>
2023-08-02 22:56:41 +0900

    recoil selector set

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { useRecoilState } from "recoil";
-import { minuteState } from "./atoms";
+import { hourSelector, minuteState } from "./atoms";
 import React from "react";
 
 function App() {
     const [minutes, setMinutes] = useRecoilState(minuteState);
+    const [hours, setHours] = useRecoilState(hourSelector);
     const onMinutesChange = (e:React.FormEvent<HTMLInputElement>) => {
         setMinutes(+e.currentTarget.value);
     }
-    console.log(minutes);
+    const onHoursChange = (e:React.FormEvent<HTMLInputElement>) => {
+        setHours(+e.currentTarget.value);
+    }
     return (
         <div>
             <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
-            <input type="number" placeholder="Hours" />
+            <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours" />
         </div>
     );
 }
